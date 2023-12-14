b4be01e740f2980a40e72925d3cb19fb3feb3844
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2023-12-14 17:16:26 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GifsModule,
+    SharedModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })

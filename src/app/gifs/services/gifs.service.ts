cf9fe6cc9bbdf2e33d16cc6c66b3fe3b2c77822f
FranciscoJimenezJimenez24 <franjimenezjimenez24@gmail.com>
2023-12-20 21:06:26 +0100

    update

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,17 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif,SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
-
+  public listaGifs:Gif[]=[]
   private _historialEtiquetas:string[]=[];
+  private apiKey:string="cfN3pXaRpMeNbpI0QOwfMRQvHslFBugY"
+  private serviceUrl:string="https://api.giphy.com/v1/gifs"
+
+  constructor(private http:HttpClient){}
 
   get historialEtiquetas(){
     return [...this._historialEtiquetas];
@@ -13,14 +19,25 @@ export class GifsService {
 
   //aqui, introduce algo en el searchbox, y se guarda en el historial (array)
   buscarEtiqueta(etiqueta:string){
-    for (let i=0;i<this._historialEtiquetas.length;i++){
-      if (this._historialEtiquetas[i]===etiqueta) {
-        delete(this._historialEtiquetas[i])
-        i=this._historialEtiquetas.length;
-      }
+    const etiquetaMins=etiqueta.toLowerCase().trim()
+
+    if (this._historialEtiquetas.includes(etiquetaMins)){
+      this._historialEtiquetas= this._historialEtiquetas.filter(busqueda => busqueda !=etiquetaMins)
     }
-    this._historialEtiquetas.unshift(etiqueta)
-    console.log(this.historialEtiquetas)
+    if (etiquetaMins.length===0){
+      return;
+    }
+    this._historialEtiquetas.unshift(etiquetaMins);
+    this._historialEtiquetas=this._historialEtiquetas.slice(0,10)
+
+    const params=new HttpParams()
+      .set('api_key',this.apiKey)
+      .set('limit',10)
+      .set('q',etiqueta);
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`,{params}).subscribe(resp=>{
+      console.log(resp);
+    })
   }
-  constructor(){}
+
 }
